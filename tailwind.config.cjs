f2eeff3463efc16aded6eae10cda9b4701ebf1ba
matej-kotrba <matejkotrba.skola@gmail.com>
2024-11-17 15:39:26 +0100

    update: added colors, fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  safelist: ['dark'],
   theme: {
+    container: {
+      screens: {
+				'2xl': '1400px'
+			}
+    },
     extend: {
-      
+      colors: {
+        background: 'hsl(var(--background) / <alpha-value>)',
+        foreground: 'hsl(var(--foreground) / <alpha-value>)',
+        primary: 'hsl(var(--primary) / <alpha-value>)',
+      }
     }
   },
   plugins: []
