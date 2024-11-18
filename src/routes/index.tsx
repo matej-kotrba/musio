8278b9ad90e2d4dab3ca8ae62379ba7bf97bb6aa
matej-kotrba <matejkotrba.skola@gmail.com>
2024-11-18 19:50:15 +0100

    wip

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,25 @@
+import index from "./index.module.css";
 import { A } from "@solidjs/router";
 
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-primary font-thin uppercase my-16">Hello world!</h1>
-      <p class="mt-8">
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+      <div class="">
+        <h1 class="text-6xl text-primary font-bold my-16">Musio</h1>
+      </div>
+      <div
+        class={`${index.host__join} relative mx-auto text-foreground text-2xl font-semibold w-[32rem] flex`}
+      >
+        <button type="button" class="grow-[1] py-8 hover:grow-[2] duration-150">
+          Host
+        </button>
+        <div
+          class={`${index.host__join__separator} w-1 bg-primary rounded-md`}
+        ></div>
+        <button type="button" class="grow-[1] py-8 hover:grow-[2] duration-150">
+          Join
+        </button>
+      </div>
     </main>
   );
 }
