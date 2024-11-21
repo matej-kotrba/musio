b2fc6157fe089fae1dd11a7e9844bcf394ef9631
matej-kotrba <matejkotrba.skola@gmail.com>
2024-11-21 21:51:14 +0100

    update: design update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main class="text-center mx-auto text-gray-700 p-4">
       <div class={index.landing}>
         <div class="py-16">
-          <h1 class="text-6xl text-primary font-bold my-16">Musio</h1>
+          <h1 class={`${index.main__title} text-6xl font-bold my-16`}>Musio</h1>
         </div>
         <div
           class={`${index.host__join} relative mx-auto text-foreground text-2xl font-semibold w-[32rem] h-24 flex bg-background-accent rounded-xl`}
