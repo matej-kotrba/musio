edd63f4b2955d3e548e25ee2fd7f20c7abaa77c1
matej-kotrba <matejkotrba.skola@gmail.com>
2024-11-21 20:01:11 +0100

    update: wip homepage

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,27 @@
 import styles from "./Nav.module.css";
 import { A } from "@solidjs/router";
+import musioLogo from "/svgs/logo.svg";
 
 export default function Nav() {
   return (
-    <nav class="bg-background_accent h-20 flex items-center gap-4 px-4 lg:px-8 2xl:px-16">
-      <A
-        href="/"
-        title="Host"
-        class={`${styles.link} font-bold text-2xl relative`}
-      >
-        Host
-      </A>
-      <A href="/" title="Join" class="font-bold text-2xl relative">
-        Join
-      </A>
+    <nav class="h-20 flex items-center justify-between px-4 lg:px-8 2xl:px-16">
+      <img src={musioLogo} alt="" class="w-16" />
+      <div class="flex gap-4">
+        <A
+          href="/"
+          title="Host"
+          class={`${styles.link} font-bold text-2xl relative hover:bg-background-accent p-2 rounded-md duration-100`}
+        >
+          Host
+        </A>
+        <A
+          href="/"
+          title="Join"
+          class={`${styles.link} font-bold text-2xl relative hover:bg-background-accent p-2 rounded-md duration-100`}
+        >
+          Join
+        </A>
+      </div>
     </nav>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,33 @@
 import index from "./index.module.css";
-import { A } from "@solidjs/router";
 
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <div class="">
-        <h1 class="text-6xl text-primary font-bold my-16">Musio</h1>
-      </div>
-      <div
-        class={`${index.host__join} relative mx-auto text-foreground text-2xl font-semibold w-[32rem] h-24 flex bg-background_accent rounded-xl`}
-      >
-        <button
-          type="button"
-          class="grow-[1] hover:grow-[2] duration-150 relative"
-        >
-          Host
-        </button>
+      <div class={index.landing}>
+        <div class="py-16">
+          <h1 class="text-6xl text-primary font-bold my-16">Musio</h1>
+        </div>
         <div
-          class={`${index.host__join__separator} w-1 bg-primary rounded-md h-[120%] translate-y-[-10%]`}
-        ></div>
-        <button
-          type="button"
-          class="grow-[1] hover:grow-[2] duration-150 relative"
+          class={`${index.host__join} relative mx-auto text-foreground text-2xl font-semibold w-[32rem] h-24 flex bg-background-accent rounded-xl`}
         >
-          Join
-        </button>
+          <button
+            type="button"
+            class="grow-[1] hover:grow-[2] duration-150 relative"
+          >
+            Host
+          </button>
+          <div
+            class={`${index.host__join__separator} w-1 bg-primary rounded-md h-[120%] translate-y-[-10%]`}
+          ></div>
+          <button
+            type="button"
+            class="grow-[1] hover:grow-[2] duration-150 relative"
+          >
+            Join
+          </button>
+        </div>
       </div>
+      <div class="pb-[200rem]"></div>
     </main>
   );
 }
