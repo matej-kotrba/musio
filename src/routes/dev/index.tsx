4b82879b5d6f1be32dfc76d49d3fc07139249a0f
matej-kotrba <matejkotrba.skola@gmail.com>
2024-11-30 23:03:20 +0100

    update: creating profile creator

--- src/routes/dev/index.tsx
+++ src/routes/dev/index.tsx
@@ -1,3 +1,38 @@
+import { getAllIcons } from "~/components/lobby/Player";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import styles from "./index.module.css";
+
 export default function Dev() {
-  return <div class="container mx-auto">asd</div>;
+  const icons = getAllIcons();
+
+  return (
+    <div class="container mx-auto">
+      <Dialog open>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Set your game profile</DialogTitle>
+            <DialogDescription>
+              <div class={`${styles.profile}`}>
+                <img
+                  src={icons[Math.round(Math.random() * icons.length)].url}
+                  alt="Player icon"
+                  class="w-40 aspect-square rounded-full mx-auto"
+                />
+              </div>
+              {/* This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers. */}
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
