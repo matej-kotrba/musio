58a556b6d6b999ae0bbbd39d658bb58e9bd58ac5
matej-kotrba <matejkotrba.skola@gmail.com>
2024-12-27 17:20:00 +0100

    update: fixed incorrect type inference

--- packages/shared/messages.ts
+++ packages/shared/messages.ts
@@ -19,14 +19,21 @@ function createNewMessage<R extends WS_MESSAGE, T extends keyof R>(
   } as WS_MessageInterface<R>[T];
 }
 
-export const createNewMessageToClient = createNewMessage<
-  WS_MessageMapServer,
-  WS_MESSAGE_TO_SERVER_TYPE
->;
-export const createNewMessageToServer = createNewMessage<
-  WS_MessageMapClient,
-  WS_MESSAGE_TO_CLIENT_TYPE
->;
+export function createNewMessageToClient<T extends WS_MESSAGE_TO_SERVER_TYPE>(
+  lobbyId: string,
+  type: T,
+  payload: WS_MessageMapServer[T]
+) {
+  return createNewMessage<WS_MessageMapServer, T>(lobbyId, type, payload);
+}
+
+export function createNewMessageToServer<T extends WS_MESSAGE_TO_CLIENT_TYPE>(
+  lobbyId: string,
+  type: T,
+  payload: WS_MessageMapClient[T]
+) {
+  return createNewMessage<WS_MessageMapClient, T>(lobbyId, type, payload);
+}
 
 export function fromMessage<T extends WS_MESSAGE>(seriliazedMessage: string) {
   return JSON.parse(seriliazedMessage) as {
