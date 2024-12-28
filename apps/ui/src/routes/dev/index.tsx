d8cb72e3f62b51129a773635298246f8353965e3
matej-kotrba <matejkotrba.skola@gmail.com>
2024-12-28 14:49:29 +0100

    update: added popover from shadcn

--- apps/ui/src/routes/dev/index.tsx
+++ apps/ui/src/routes/dev/index.tsx
@@ -1,3 +1,27 @@
+import {
+  TextField,
+  TextFieldLabel,
+  TextFieldRoot,
+} from "~/components/ui/textfield";
+
 export default function Dev() {
-  return <div></div>;
+  return (
+    <div>
+      <div class="w-80 mx-auto">
+        <TextFieldRoot>
+          <TextFieldLabel for="name" class="block text-center">
+            Pick song for others to guess:
+          </TextFieldLabel>
+          <TextField
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={""}
+            min={1}
+            class="text-lg py-6"
+          />
+        </TextFieldRoot>
+      </div>
+    </div>
+  );
 }
