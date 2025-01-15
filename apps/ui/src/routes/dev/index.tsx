fe60398a94fd2368ddcb897092b840ef8cd7d21f
matej-kotrba <matejkotrba.skola@gmail.com>
2025-01-15 22:19:13 +0100

    wip

--- apps/ui/src/routes/dev/index.tsx
+++ apps/ui/src/routes/dev/index.tsx
@@ -1,3 +1,4 @@
+import styles from "./styles.module.css";
 import clsx from "clsx";
 import { Player } from "shared";
 import { Index } from "solid-js";
@@ -49,60 +50,48 @@ const dummy_players: Player[] = [
 ];
 
 export default function Dev() {
-  const highestScore = () => {
-    let highest = 0;
-    dummy_players.forEach((player) => {
-      if (player.points > highest) highest > highest;
-    });
-    return highest;
-  };
-
   return (
     <div class="container mx-auto mt-20">
-      <div class="flex h-[200px] gap-1">
-        <Index each={dummy_players}>
-          {(player, idx) => {
-            return <PlayerInScoreboard player={player()} height={highestScore()} />;
-          }}
-        </Index>
+      <div class="mx-auto w-fit h-[300px] grid grid-cols-3 grid-rows-4 gap-4">
+        <PlayerOnTopThree player={dummy_players[1]} class="row-span-3 row-start-2 col-start-1" />
+        <PlayerOnTopThree player={dummy_players[0]} class="row-span-4 col-start-2" />
+        <PlayerOnTopThree player={dummy_players[2]} class="row-span-2 row-start-3 col-start-3" />
       </div>
-      {/* <div class="w-fit h-[300px] grid grid-cols-3 grid-rows-4 gap-4">
-        <PlayerInScoreboard player={dummy_players[1]} class="row-span-3 row-start-2 col-start-1" />
-        <PlayerInScoreboard player={dummy_players[0]} class="row-span-4 col-start-2" />
-        <PlayerInScoreboard player={dummy_players[2]} class="row-span-2 row-start-3 col-start-3" />
-        <Index each={dummy_players}>
-          {(player, idx) => {
-            return <PlayerInScoreboard player={player()} index={idx} />;
-          }}
-        </Index>
-      </div> */}
+      <Index each={dummy_players.toSpliced(0, 3)}>
+        {(player) => {
+          return <PlayerBelowTopThree player={player()} />;
+        }}
+      </Index>
     </div>
   );
 }
 
-type PlayerInScoreboardProps = {
+type PlayerComponentProps = {
   player: Player;
-  height: number;
   class?: string;
 };
 
-function PlayerInScoreboard(props: PlayerInScoreboardProps) {
-  return <div class={clsx("", props.class)}></div>;
+function PlayerOnTopThree(props: PlayerComponentProps) {
+  return (
+    <div class={clsx("flex", props.class)}>
+      <p
+        class={`${styles.leaderboards__player_name} [writing-mode:sideways-lr] text-ellipsis whitespace-nowrap overflow-hidden text-lg font-bold`}
+      >
+        {props.player.name}
+      </p>
+      <div class={"from-transparent to-primary/80 bg-gradient-to-t h-full rounded-t-md"}>
+        <img
+          src={props.player.icon.url}
+          alt=""
+          width={96}
+          height={96}
+          class="w-24 aspect-square rounded-md shadow-lg shadow-black/50 translate-x-1"
+        />
+      </div>
+    </div>
+  );
 }
 
-// type PlayerInScoreboardProps = {
-//   player: Player;
-//   class?: string;
-// };
-
-// function PlayerInScoreboard(props: PlayerInScoreboardProps) {
-//   return (
-//     <div class={clsx("flex", props.class)}>
-//       <p class="text-ellipsis whitespace-nowrap overflow-hidden">{props.player.name}</p>
-//       <div class={"bg-red-500 h-full"}>
-//         <img src={props.player.icon.url} alt="" width={80} height={80} class="rounded-md" />
-//         {/* <span>{props.player.name}</span> */}
-//       </div>
-//     </div>
-//   );
-// }
+function PlayerBelowTopThree(props: PlayerComponentProps) {
+  return <div>{props.player.name}</div>;
+}
