b07e8cffce04203c5a444e963f24e2b8603cbaf3
matej-kotrba <matejkotrba.skola@gmail.com>
2025-08-28 19:27:22 +0200

    feat: phase handler type

---
+++ apps/backend/src/lib/events/phasesHandlers/phasesHandler.ts
@@ -0,0 +1,52 @@
+import type { FromMessageOnServer, WS_MessageMapClient } from "shared";
+
+interface Mapping {
+  A: "a";
+  B: "b";
+  C: "c";
+  D: "d";
+  E: "e";
+  F: "f";
+  G: "g";
+  H: "h";
+  I: "i";
+  J: "j";
+  K: "k";
+  L: "l";
+  M: "m";
+  N: "n";
+  O: "o";
+  P: "p";
+  Q: "q";
+  R: "r";
+  S: "s";
+  T: "t";
+  U: "u";
+  V: "v";
+  W: "w";
+  X: "x";
+  Y: "y";
+  Z: "z";
+}
+
+type MappingKeys = keyof Mapping;
+
+type Convert<
+  T extends string,
+  Base extends string = "",
+  IsFirst extends boolean = true
+> = T extends `${infer Char}${infer Rest}`
+  ? Char extends "_"
+    ? Convert<Rest, Base, true>
+    : Char extends MappingKeys
+    ? IsFirst extends true
+      ? Convert<Rest, `${Base}${Char}`, false>
+      : Convert<Rest, `${Base}${Mapping[Char]}`, false>
+    : Convert<Rest, `${Base}${Char}`, false>
+  : Base;
+
+export type PhasesHandlerMethods = {
+  [Key in keyof WS_MessageMapClient as `handle${Convert<Key>}`]?: (
+    eventData: Extract<FromMessageOnServer, { message: { type: Key } }>
+  ) => void;
+};
