ed7a9e75798481e92f7abb34656139399d537951
matej-kotrba <matejkotrba.skola@gmail.com>
2025-09-21 12:49:58 +0200

    feat: added error boundary around backend accessibility check

--- apps/ui/src/features/checkBackendAccessibility/components/BackendAccessibilityDisplay.tsx
+++ apps/ui/src/features/checkBackendAccessibility/components/BackendAccessibilityDisplay.tsx
@@ -1,6 +1,6 @@
 import { Icon } from "@iconify-icon/solid";
 import { constructURL, getServerURL } from "shared";
-import { createResource, Show } from "solid-js";
+import { createResource, ErrorBoundary, Show } from "solid-js";
 import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
 
 const pingServerUrl = () => constructURL(getServerURL(import.meta.env.VITE_ENVIRONMENT), "ping");
@@ -14,14 +14,22 @@ export default function BackendAccessibilityDisplay() {
   const [data] = createResource(pingBackend);
 
   return (
-    <Show when={data() !== 200}>
-      <Alert variant={"destructive"} class="bg-secondary">
-        <AlertTitle class="text-xl leading-10 flex items-center">
-          <Icon icon={"lucide:triangle-alert"} class="text-2xl text-destructive" />
-          <span class="ml-2">Heads up!</span>
-        </AlertTitle>
-        <AlertDescription>We apologize but our services are currently down!</AlertDescription>
-      </Alert>
-    </Show>
+    <ErrorBoundary fallback={<ServicesDownAlert />}>
+      <Show when={data() !== 200}>
+        <ServicesDownAlert />
+      </Show>
+    </ErrorBoundary>
+  );
+}
+
+function ServicesDownAlert() {
+  return (
+    <Alert variant={"destructive"} class="bg-secondary">
+      <AlertTitle class="text-xl leading-10 flex items-center">
+        <Icon icon={"lucide:triangle-alert"} class="text-2xl text-destructive" />
+        <span class="ml-2">Heads up!</span>
+      </AlertTitle>
+      <AlertDescription>We apologize but our services are currently down!</AlertDescription>
+    </Alert>
   );
 }
