c2055af893864ee9233d77b2106d1ec5e66c034a
matej-kotrba <matejkotrba.skola@gmail.com>
2025-10-31 20:17:19 +0100

    cleanup

--- apps/ui/src/utils/rscs.ts
+++ apps/ui/src/utils/rscs.ts
@@ -19,17 +19,3 @@ export const getLobbyURL = async (serverUrl?: string, lobbyId?: string) => {
 
   return data;
 };
-
-// // Removes lobby from lobbies map if the lobby is empty
-// export const removeLobbyWhenEmpty = (lobbyId: string) => {
-//   // console.log("Removing lobby", lobbyId);
-//   const lobby = getLobbies().get(lobbyId);
-
-//   // console.log(lobbies, lobby);
-//   if (!lobby || lobby.players.length > 0) return;
-//   getLobbies().delete(lobbyId);
-
-//   for (const [id, name] of getLobbies()) {
-//     console.log(id, name);
-//   }
-// };
